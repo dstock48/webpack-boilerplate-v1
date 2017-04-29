3cbf8f9c485bd4a4b61de42fd6fa7fcdb7399455
Dave W <dviper@gmail.com>
2017-04-29 17:02:16 -0600

    style updates

--- src/app.js
+++ src/app.js
@@ -3,5 +3,9 @@ const css = require('./styles.scss');
 /* eslint-enable */
 
 const name = 'Dave';
+console.log(`Hey, look ${name}... it worked!`);
 
-console.log(`Hey look ${name}, it worked!`);
+setInterval(() => {
+  const unorderedList = document.querySelector('ul');
+  unorderedList.classList.toggle('color-border');
+}, 600);
